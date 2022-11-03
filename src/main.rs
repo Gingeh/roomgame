89dc0a498d9a5074174b43c5a999218865881349
Gingeh <39150378+Gingeh@users.noreply.github.com>
2022-11-04 09:40:01 +1100

    Load gltf

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,17 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 2.2, 0.0).looking_at(Vec3::new(-1.6, 1.6, 0.0), Vec3::Y),
+        ..default()
+    });
+    commands.spawn_bundle(SceneBundle {
+        scene: asset_server.load("panel.gltf#Scene0"),
+        ..default()
+    });
 }
