ae1c2c3270b09f5695c4f5a484d4bc5260bf52f5
Gingeh <39150378+Gingeh@users.noreply.github.com>
2022-11-04 10:14:26 +1100

    Simple desk mesh

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
-use bevy::prelude::*;
+use std::f32::consts::PI;
+
+use bevy::prelude::{shape::Box, *};
+
+#[derive(Component)]
+struct Desk;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(Camera3dBundle {
+        ..Default::default()
+    });
+
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Box::new(2.0, 1.0, 1.0).into()),
+            material: materials.add(Color::ANTIQUE_WHITE.into()),
+            transform: Transform::from_translation(Vec3::new(0.0, -0.6, -2.0))
+                .with_rotation(Quat::from_rotation_x(PI / 6.0)),
+            ..Default::default()
+        })
+        .insert(Desk);
 }
