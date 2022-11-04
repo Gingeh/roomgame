503fe2b555d6cc18932c84e834ee95686d61b223
Gingeh <39150378+Gingeh@users.noreply.github.com>
2022-11-04 21:28:44 +1100

    Add some comments

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use std::f32::consts::PI;
 use bevy::prelude::{shape::Box, *};
 use bevy_inspector_egui::WorldInspectorPlugin;
 
+/// Marker component for the desk/panel thing
 #[derive(Component)]
 struct Desk;
 
+/// Marker component for the spotlight
 #[derive(Component)]
 struct Lamp;
 
+/// Marker component for the buttons
 #[derive(Component)]
 enum Button {
     Red,
@@ -21,20 +24,24 @@ fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
+        //TODO: Feature gate the inpector
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
         .run();
 }
 
+/// Spawn the camera and panel
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // Camera
     commands.spawn_bundle(Camera3dBundle {
         ..Default::default()
     });
 
+    // Desk
     commands
         .spawn_bundle(PbrBundle {
             mesh: meshes.add(Box::new(2.0, 1.0, 1.0).into()),
@@ -45,6 +52,7 @@ fn setup(
         })
         .insert(Desk)
         .with_children(|parent| {
+            // Desk lamp
             parent.spawn_bundle(SpotLightBundle {
                 spot_light: SpotLight {
                     intensity: 100.0,
@@ -58,6 +66,7 @@ fn setup(
                 ..Default::default()
             }).insert(Lamp);
 
+            // Buttons
             //TODO: Can this be refactored?
             parent.spawn_bundle(PbrBundle {
                 mesh: meshes.add(Box::new(1.0, 1.0, 1.0).into()),
